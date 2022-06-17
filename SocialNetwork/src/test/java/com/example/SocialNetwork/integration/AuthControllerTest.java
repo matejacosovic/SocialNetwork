1ae7a164801c20847d652c73ebe58c6822d9204b
mateja.cosovic <mateja.cosovic@vegait.rs>
2022-06-17 13:32:01 +0200

    OM-24: adds code coverage to sonarqube report

--- SocialNetwork/src/test/java/com/example/SocialNetwork/integration/AuthControllerTest.java
+++ SocialNetwork/src/test/java/com/example/SocialNetwork/integration/AuthControllerTest.java
@@ -1,14 +1,44 @@
 package com.example.SocialNetwork.integration;
 
 import com.example.SocialNetwork.domain.dto.JwtAuthDTO;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.equalTo;
 
-public class AuthControllerTest extends AbstractControllerTest {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class AuthControllerTest extends TestMapper {
+
+    @Autowired
+    protected WebApplicationContext context;
+    @Autowired
+    protected MockMvc mvc;
+
+    @Before
+    public void setup() {
+        this.mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
+    }
 
     @Test
     public void login_validCredentials_statusOk() throws Exception {
